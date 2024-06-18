971a6206fdb021a269683519777df4dfc86ae5d0
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-06-18 18:43:09 -0500

    improve basic landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
-import { Metadata } from "next";
-
 // json data for mock testimonials
 import testimonials from "@/local_data/testimonials.json";
+import { getIsAuth } from "@/utils";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Bark Avenue Orphanage | Home",
   description: "Adopting smiles, One paw at a time. &#x1F43E;",
 };
 
-export default function Home() {
+export default async function Home() {
+  const isAuth = await getIsAuth();
+
+  console.log("auth", isAuth);
+
   return (
     <div className="flex h-full flex-col gap-3 bg-white p-5">
       <section id="hero" className="flex h-1/2 flex-col gap-4 py-5">
-        <span className="rounded border border-orange-300 bg-orange-100 p-1 text-center text-sm text-orange-600">
-          to see dogs available to adopt, please sign in
-        </span>
+        {!isAuth && (
+          <span className="rounded border border-orange-300 bg-orange-100 p-1 text-center text-sm text-orange-600">
+            to see dogs available to adopt, please sign in
+          </span>
+        )}
         <div>
           <h1 className="py-3 text-4xl font-bold">
             Welcome to Bark Avenue Orphanage.
@@ -26,6 +32,10 @@ export default function Home() {
       </section>
       <section id="testemonials">
         <h2 className="pb-2 pt-3 text-2xl font-bold">Testimonials</h2>
+        <p className="px-2 py-1">
+          Our testimonials show we are definitely barking up the right tree.
+          Hear what our dogs have had to say!
+        </p>
         <ul className="flex flex-col gap-2 text-black">
           {testimonials.map((testimonial) => (
             <li
