a90759e8b959e675bcf169748156ce952ff099f0
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-06-19 10:51:05 -0500

    name change?

--- utils.ts
+++ utils.ts
@@ -5,6 +5,6 @@ export const getBaseURL = async () => process.env.BASE_URL;
 // server check for specific auth cookie, used across app for checks
 export const getIsAuth = () => {
   const cookieStore = cookies();
-  const access = cookieStore.get("Secure, fetch-access-token");
+  const access = cookieStore.get("fetch-access-token");
   return Boolean(access);
 };
