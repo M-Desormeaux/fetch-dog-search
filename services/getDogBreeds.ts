a88d7d668a477054352e806e562fa393b554c0f0
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-06-19 12:41:34 -0500

    updated apis

--- services/getDogBreeds.ts
+++ services/getDogBreeds.ts
@@ -1,21 +1,25 @@
 // "use client";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const getDogBreeds = async () => {
   const url = "https://frontend-take-home-service.fetch.com";
 
-  // const cookieStore = cookies().toString();
+  const cookieStore = cookies().toString();
+
+  console.log(cookieStore);
 
   const response = await fetch(url + "/dogs/breeds", {
     method: "GET",
     credentials: "include",
+    cache: "no-store",
     headers: {
       "Content-Type": "application/json",
-      // Cookie: cookieStore,
+      Cookie: cookieStore,
     },
   });
 
   // if (!response.ok) redirect("/login?error=fail");
 
-  return response.status;
+  return response.json();
 };
--- services/postLogIn.ts
+++ services/postLogIn.ts
@@ -1,4 +1,5 @@
 // "use client";
+import { cookies } from "next/headers";
 
 export const postLogIn = async (args: { name: string; email: string }) => {
   const url = "https://frontend-take-home-service.fetch.com";
@@ -14,6 +15,34 @@ export const postLogIn = async (args: { name: string; email: string }) => {
 
   if (!response.ok) {
     return false;
+  }
+
+  const setCookieHeader = response.headers.get("set-cookie");
+
+  if (setCookieHeader) {
+    // Parse the specific cookie value if needed
+    const cookiesHeaders = setCookieHeader
+      .split(";")
+      .map((cookie) => cookie.trim());
+    const specificCookie = cookiesHeaders.find((cookie) =>
+      cookie.startsWith("Secure, fetch-access-token="),
+    );
+
+    // grab and set cookie manually since for some reason credentials and axios do not work as well with next 14
+    const realCookie = specificCookie?.split("=")[1] ?? "";
+
+    cookies().set("fetch-access-token", realCookie, {
+      httpOnly: true,
+      secure: true,
+    });
+
+    if (specificCookie) {
+      console.log("\n\nSpecific Cookie:", realCookie, "\n\n");
+    } else {
+      console.log("Specific cookie not found");
+    }
+  } else {
+    console.log("No Set-Cookie header found");
   }
 
   return true;
