dbbca83769561725d9be751eadbec14ec8b2255f
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-06-19 15:05:59 -0500

    updated nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,3 @@
-import { AuthButton } from "./AuthButton";
 import { getIsAuth } from "@/utils";
 import Link from "next/link";
 
@@ -8,6 +7,7 @@ export const Nav = async () => {
   but just want to hide it on /login, so login button is client to check pathname
   */
   const isAuth = getIsAuth();
+  const color = isAuth ? "bg-red-300 text-white" : "bg-green-300 text-black";
 
   return (
     <nav className="flex min-h-14 w-full items-center justify-between gap-2 border-b bg-white px-3">
@@ -17,7 +17,15 @@ export const Nav = async () => {
       >
         Home
       </Link>
-      <AuthButton isAuth={isAuth} />
+      <Link
+        href={isAuth ? "/logout" : "/login"}
+        className={
+          "drop-shadow-sm hover:drop-shadow-md active:drop-shadow h-min rounded border bg-gray-50 px-3 py-2 font-semibold " +
+          color
+        }
+      >
+        {isAuth ? "Logout" : "Login"}
+      </Link>
     </nav>
   );
 };
