c1feb43e6b7a1045334ad2e11d659a2c98211549
Michael Desormeaux <mdesormeaux96@gmail.com>
2024-06-19 11:48:09 -0500

    prune api services

--- services/getDogBreeds.ts
+++ services/getDogBreeds.ts
@@ -1,23 +1,22 @@
-import { getBaseURL } from "@/utils";
-import { cookies } from "next/headers";
+"use client";
 
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export const getDogBreeds = async () => {
-  const url = await getBaseURL();
+  const url = "https://frontend-take-home-service.fetch.com";
 
-  const cookieStore = cookies().toString();
+  // const cookieStore = cookies().toString();
 
   const response = await fetch(url + "/dogs/search?size=10", {
     method: "GET",
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
-      Cookie: cookieStore,
+      // Cookie: cookieStore,
     },
   });
 
   // if (!response.ok) redirect("/login?error=fail");
 
-  return response;
+  return response.ok;
 };
--- services/postLogIn.ts
+++ services/postLogIn.ts
@@ -1,10 +1,7 @@
-import { getBaseURL } from "@/utils";
-import { cookies } from "next/headers";
+"use client";
 
 export const postLogIn = async (args: { name: string; email: string }) => {
-  const url = await getBaseURL();
-
-  const cookieStore = cookies().toString();
+  const url = "https://frontend-take-home-service.fetch.com";
 
   const response = await fetch(url + "/auth/login", {
     method: "POST",
@@ -12,42 +9,11 @@ export const postLogIn = async (args: { name: string; email: string }) => {
     credentials: "include",
     headers: {
       "Content-Type": "application/json",
-      Cookie: cookieStore,
     },
   });
 
   if (!response.ok) {
     return false;
-  }
-
-  // Some checks for if the cookie is there for quick debugging.
-  // Access the `Set-Cookie` header from the response (server-side)
-  const setCookieHeader = response.headers.get("set-cookie");
-
-  if (setCookieHeader) {
-    // Parse the specific cookie value if needed
-    const cookiesHeaders = setCookieHeader
-      .split(";")
-      .map((cookie) => cookie.trim());
-    const specificCookie = cookiesHeaders.find((cookie) =>
-      cookie.startsWith("Secure, fetch-access-token="),
-    );
-
-    // grab and set cookie manually since for some reason credentials and axios do not work as well with next 14
-    const realCookie = specificCookie?.split("=")[1] ?? "";
-
-    cookies().set("fetch-access-token", realCookie, {
-      httpOnly: true,
-      secure: true,
-    });
-
-    if (specificCookie) {
-      console.log("\n\nSpecific Cookie:", realCookie, "\n\n");
-    } else {
-      console.log("Specific cookie not found");
-    }
-  } else {
-    console.log("No Set-Cookie header found");
   }
 
   return true;
